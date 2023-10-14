1dc5121c2f35d442528c60cc73769de6aa739e9e
bnur-yildirim <128072981+bnur-yildirim@users.noreply.github.com>
2023-10-14 09:53:46 +0300

    Update my-first-list.c

--- Singly Linked List/my-first-list.c
+++ Singly Linked List/my-first-list.c
@@ -9,7 +9,7 @@
 /* C program to create a singly linked list */
 
 struct Node {
-	int data;        
+	int data;
 	struct Node* next;				//pointer to the next node
 };
 
@@ -18,12 +18,9 @@ int main(void)
 	struct Node* head;            //head pointer
 	// allocate memory for the node
 	head = (struct Node*)malloc(sizeof(struct Node));
-	
+
 	head->data = 45;     // first member in the list is 45
 	head->next = NULL;   // there is only one node so it points to null
-
-	//printing list
-	printf("%d ", head->data);
 
 	//creating second node
 	struct Node* current = (struct Node*)malloc(sizeof(struct Node));
@@ -32,6 +29,22 @@ int main(void)
 
 	//connecting nodes
 	head->next = current;
+
+	//creating third node
+	current = (struct Node*)malloc(sizeof(struct Node));
+	current->data = 3;
+	current->next = NULL;
+
+	//connecting nodes
+	head->next->next = current;
+
+	//print list
+	current = head;
+	while (current != NULL)
+	{
+		printf("%d ", current->data);
+		current = current->next;
+	}
 
 	return (0);
 }
