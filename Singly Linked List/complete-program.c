2097d83e42ad1f93b47a01e155ecf010759c5c67
bnur-yildirim <128072981+bnur-yildirim@users.noreply.github.com>
2023-10-15 15:28:41 +0300

    Update and rename insert-at-the-end.c to complete-program.c

--- Singly Linked List/complete-program.c
+++ Singly Linked List/complete-program.c
@@ -6,7 +6,7 @@
 	Last Update : 15.10.2023
 */
 
-/* program to create list and insert a node at the end */
+
 
 struct Node {
 	int data;
@@ -17,21 +17,30 @@ void createL(struct Node** head);
 
 void printL(struct Node* head);
 
+void insertNode(struct Node** head);
+
 int main(void) {
+
 	int count;
 	struct Node* head = (struct Node*)malloc(sizeof(struct Node));
 	head = NULL;
 
 	printf("Enter number of nodes to be created: ");
 	scanf("%d", &count);
-    
+
 	for (int i = 0; i < count; i++) {
 		createL(&head);
 	}
+
 	printL(head);
+	insertNode(&head);
+	printL(head);
+
 	return 0;
 }
+//create list with insertion at the end
 void createL(struct Node** head) {
+	
 	int num;
 	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node*));
 	struct Node* curr = *head;   //current node
@@ -58,6 +67,7 @@ void createL(struct Node** head) {
 	}
 }
 void printL(struct Node* head) {
+
 	struct Node* temp = head;
 	if (temp == NULL)
 		return NULL;
@@ -66,5 +76,54 @@ void printL(struct Node* head) {
 	while (temp != NULL) {
 		printf("%d ", temp->data);
 		temp = temp->next;
+	}
+	printf("\n");
+}
+//insert node at any position
+void insertNode(struct Node** head) {
+	int position, num, count = 1;
+
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	struct Node* curr = *head;     //current node
+	struct Node* prev = *head;    //previous node
+	struct Node* countP = *head;   //to count number of elements in the list
+
+	if (newNode == NULL)
+		return NULL;
+
+	printf("We will insert a node.\nEnter data: ");
+	scanf("%d", &num);
+
+	newNode->data = num;    //fill data for the new node
+
+	printf("Enter position you want to insert: ");
+	scanf("%d", &position);
+	//itarate prev to position we want to insert 
+	for (int i = 1; curr->next!=NULL && i < position; i++) {
+		prev = curr;
+		curr = curr->next;
+	}
+	while (countP->next != NULL) {
+		count++;
+		countP = countP->next;
+	}
+
+	if (position==1) {
+		//insert at the beginning
+		newNode->next = *head;
+		*head = newNode;
+	}
+	else {
+		
+		//insert at the end
+		if (position==count+1) {
+			newNode->next = NULL;
+			curr->next = newNode;
+		}
+		//insert at middle
+		else {   
+			newNode->next = prev->next;
+			prev->next = newNode;
+		}	
 	}
 }
