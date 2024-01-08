cbf4c65a8ebbd021c5440f5fa17439d835ab3096
bnur-yildirim <128072981+bnur-yildirim@users.noreply.github.com>
2024-01-08 15:31:00 +0300

    Create selectionSort.cpp

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,13 @@
+void selectionSort(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		int min = i;
+		for (int j = i + 1; j < size; j++) {
+			if (arr[min] > arr[j]) {
+				min = j;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = arr[min];
+		arr[min] = temp;
+	}
+}
